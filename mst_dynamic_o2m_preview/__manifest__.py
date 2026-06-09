78fac02acbc01a6de803550d7aa5d662c54c59f2
Naresh P <Naresh.P@mindsparktechnologies.com>
2026-06-09 14:51:27 +0530

    updated one2many preview manifest file

--- mst_dynamic_o2m_preview/__manifest__.py
+++ mst_dynamic_o2m_preview/__manifest__.py
@@ -1,28 +1,33 @@
+# -*- coding: utf-8 -*-
 {
-    "name": "Dynamic One2Many Preview",
-    "version": "19.0.1.0.0",
-    "category": "Tools",
-    "summary": "Show One2Many records preview in list view",
-    "description": """
-        Dynamic One2Many Preview Widget for Odoo 19.
-        Shows selected one2many line fields inside a compact table preview.
+    'name': 'Dynamic One2many Preview Widget',
+    'version': '19.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Display One2many records dynamically inside parent list views',
+    'description': """
+        Dynamic One2many Preview Widget for Odoo 19
+        ===========================================
+        This module provides a reusable backend widget to display One2many records
+        inside parent list views using configurable XML options.
     """,
-    "author": "Mind Spark Technologies",
-    "depends": [
-        "web",
-        "sale",
+    'author': 'Mind Spark Technologies',
+    'website': 'https://www.mindsparktechnologies.com',
+    'depends': ['web','sale_management'],
+    'data': [
+        'views/example_usage.xml',
+        'views/sale_order_view.xml'
     ],
-    "assets": {
-        "web.assets_backend": [
-            "mst_dynamic_o2m_preview/static/src/js/dynamic_one2many_preview.js",
-            "mst_dynamic_o2m_preview/static/src/xml/dynamic_one2many_preview.xml",
-            "mst_dynamic_o2m_preview/static/src/css/dynamic_one2many_preview.css",
+    'assets': {
+        'web.assets_backend': [
+            'mst_dynamic_o2m_preview/static/src/js/dynamic_one2many_preview.js',
+            'mst_dynamic_o2m_preview/static/src/xml/dynamic_one2many_preview.xml',
+            'mst_dynamic_o2m_preview/static/src/css/dynamic_one2many_preview.css',
         ],
     },
-    "data": [
-        "views/sale_order_view.xml",
+    "images": [
+        "static/description/banner.png",
     ],
-    "installable": True,
-    "application": False,
-    "license": "LGPL-3",
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
 }
